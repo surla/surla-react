7814564e883d60100429608015790cdbe1bdc76a
Joseph Surla <josephsurla@gmail.com>
2018-05-24 12:05:36 -0700

    Insert website logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import './App.css';
 
 import Header from './components/Header';
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={logo} alt="Three dots logo" />
         <Header />
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: grid;
+  font-size: 2em;
+`;
+
+const Intro = styled.p`
+  align-self: end;
+  font-size: 1em;
+`
+
+
 
 const Header = () => (
-  <div>
-    <p>Hello, I'm Joseph Surla<br/>I love building new things with code.<br/></p>
+  <Wrapper>
+    <Intro>Hello, I'm Joseph Surla<br/>and I love building new things with code.<br/></Intro>
     <p>I'm what you call a full stack developer—<br/>working with Javascript and Ruby.</p>
-    <p>I love building new things with code.<br/></p>
-  </div>
+    <p>&mdash;</p>
+  </Wrapper>
 )
 
 export default Header;
