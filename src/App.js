4bcb1899e2853d8a91eaf4e119fac6c4c73c8257
Joseph Surla <josephsurla@gmail.com>
2018-05-24 13:50:04 -0700

    Move logo to Logo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './assets/images/logo.svg';
 import './App.css';
 
+import Logo from './components/Logo'
 import Header from './components/Header';
 import Projects from './components/Projects';
 
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} alt="Three dots logo" />
+        <Logo />
         <Header />
         <Projects />
       </div>
---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import logo from '../assets/images/logo.svg';
+
+const Image = styled.div`
+  margin-top: 3em;
+  margin-left: 2em;
+`
+
+const Logo = () => (
+  <Image>
+    <img src={logo} alt="three dots logo"/>
+  </Image>
+)
+
+export default Logo;
