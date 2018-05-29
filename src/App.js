d97562307b79b90b06986872f57e1ae78b3c2b63
Joseph Surla <josephsurla@gmail.com>
2018-05-29 13:46:49 -0700

    Create Footer component for copyright

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Logo from './components/Logo'
+import Logo from './components/Logo';
 import Header from './components/Header';
 import Projects from './components/Projects/Projects';
-import Contact from './components/Contact'
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Header />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Copyright = styled.p`
+  text-align: center;
+`
+
+const Footer = () => (
+  <Copyright>
+    Copyright &copy; Made by Joseph Surla
+  </Copyright>
+)
+
+export default Footer;
